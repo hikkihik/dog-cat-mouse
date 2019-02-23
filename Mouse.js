ef0c47dddcea97e259de6aaafbc91b3cf745f449
Duy Tuan <tangduytuan1997@gmail.com>
2019-02-23 21:46:18 +0700

    Revert "Add die method for Mouse"
    This reverts commit eff30622daf055d5359d45c1ae68b384c02b99b6.

--- Mouse.js
+++ Mouse.js
@@ -1,11 +1,6 @@
 function Mouse(name, color) {
 	this.name = name;
 	this.color = color;
-	this.dead = false;
 }
-
-Mouse.prototype.die = function() {
-	this.dead = true;
-};
 
 module.exports = Mouse;
